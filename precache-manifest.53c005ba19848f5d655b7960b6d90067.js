566d23d49edfe7b0b48816139dc8788302487f62
unknown <you@example.com>
2021-01-15 18:36:03 +0200

    Updates

---
+++ precache-manifest.53c005ba19848f5d655b7960b6d90067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21a4e4032fad5c722d7502304652c9b",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "0001cad0c2a85d23ff2a",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.fabd7171.chunk.css"
+  },
+  {
+    "revision": "3a439a8371800322061c",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.2df2050d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.2df2050d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0001cad0c2a85d23ff2a",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.f66b6c76.chunk.js"
+  },
+  {
+    "revision": "3f441844d3ba7dba1f3a",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.61bf2916.js"
+  }
+]);
